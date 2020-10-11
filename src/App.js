e0580fc39e193d987b20b367f8c81aa8a1521f5b
Josh Attenberg <josh.attenberg@gmail.com>
2020-10-11 11:35:17 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Map } from 'immutable';
+import rd3 from 'react-d3-library'
 import './App.css';
+
+// generator for all odd numbers
+function* odds() {
+  let i = 1;
+  while (true) {
+    yield i;
+    i += 2;
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+
       </header>
     </div>
   );
